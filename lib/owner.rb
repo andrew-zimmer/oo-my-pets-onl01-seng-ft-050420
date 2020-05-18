34a0959c781f1cf09ffb30fe6943d22b320d62c1
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-18 22:43:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,8 +29,7 @@ class Owner
   end 
   
   def cats 
-    @cats << Cat.all.select {|cat| cat.owner == self} 
-    @cats.flatten.uniq
+    Cat.all.select {|cat| cat.owner == self} 
   end
   
   def dogs
